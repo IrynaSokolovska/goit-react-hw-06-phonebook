9f705297151c0613c1bc7c6a64a833027c8f92b9
Iryna Sokolovska <sokirina777@gmail.com>
2023-10-04 21:45:01 +0300

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Account } from './Account';
+import { LangSwitcher } from './LangSwitcher';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <LangSwitcher />
+      <Account />
+    </>
   );
 };
---
+++ src/redux/store.js
@@ -0,0 +1,49 @@
+import { createStore, combineReducers } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+import { accountReducer } from './accountSlice';
+import { localeReducer } from './localeSlice';
+
+const initialState = {
+  account: {
+    balance: 500,
+  },
+  locale: {
+    lang: 'uk',
+  },
+};
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  locale: localeReducer,
+});
+
+const enhancer = devToolsEnhancer();
+
+export const store = createStore(rootReducer, enhancer);
+// const deposit = {
+//   type: 'account/deposit',
+//   payload: 10,
+// };
+
+// const rootReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'account/deposit':
+//       return {
+//         ...state,
+//         account: {
+//           ...state.account,
+//           balance: state.account.balance + action.payload,
+//         },
+//       };
+//     case 'account/withdraw':
+//       return {
+//         ...state,
+//         account: {
+//           ...state.account,
+//           balance: state.account.balance - action.payload,
+//         },
+//       };
+//     default:
+//       return state;
+//   }
+// };
