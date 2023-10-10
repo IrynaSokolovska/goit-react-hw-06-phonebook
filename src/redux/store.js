8fff3393a2e2a2beadceef8c061d868e233f1c7b
Iryna Sokolovska <sokirina777@gmail.com>
2023-10-10 18:17:40 +0300

    six

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-// import { createReducer } from '@reduxjs/toolkit';
+
 import contactsReducer from './contactsSlice';
 import { filterReducer } from './filterSlice';
 import {
@@ -21,9 +21,6 @@ const rootReducer = combineReducers({
 const persistConfig = { key: 'root', storage };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-// // import { devToolsEnhancer } from '@redux-devtools/extension'; вже не потрібен
-// import { accountReducer } from './accountSlice';
-// import { localeReducer } from './localeSlice';
 
 export const store = configureStore({
   reducer: persistedReducer,
@@ -35,54 +32,3 @@ export const store = configureStore({
     }),
 });
 export const persistor = persistStore(store);
-
-// -----------------------------------------------------------------------------
-// export const store = createStore(rootReducer, enhancer);    вже не потрібен
-// const deposit = {
-//   type: 'account/deposit',
-//   payload: 10,
-// };
-
-// const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case 'account/deposit':
-//       return {
-//         ...state,
-//         account: {
-//           ...state.account,
-//           balance: state.account.balance + action.payload,
-//         },
-//       };
-//     case 'account/withdraw':
-//       return {
-//         ...state,
-//         account: {
-//           ...state.account,
-//           balance: state.account.balance - action.payload,
-//         },
-//       };
-//     default:
-//       return state;
-//   }
-// };
-// -----------------------------------------------------------------------------
-// const initialState = {
-//   account: {
-//     balance: 500,
-//   },
-//   locale: {
-//     lang: 'uk',
-//   },
-// };
-
-// const rootReducer = combineReducers({
-//   account: accountReducer,
-//   locale: localeReducer,
-// });
-
-// const enhancer = devToolsEnhancer(); вже не потрібен
-
-// export const store = configureStore({
-//   reducer: rootReducer,
-// });
-// -----------------------------------------------------------------------
